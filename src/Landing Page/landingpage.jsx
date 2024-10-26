0584707856803c733ae51c844a80f3aeb8eb097b
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-26 11:31:18 +0530

    removed credentials

--- src/Landing Page/landingpage.jsx
+++ src/Landing Page/landingpage.jsx
@@ -2,15 +2,7 @@ import React, { useEffect } from 'react'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-const firebaseConfig = {
-  apiKey: "AIzaSyA5h_ElqdgLrs6lXLgwHOfH9Il5W7ARGiI",
-  authDomain: "vistafeedd.firebaseapp.com",
-  projectId: "vistafeedd",
-  storageBucket: "vistafeedd.appspot.com",
-  messagingSenderId: "1025680611513",
-  appId: "1:1025680611513:web:40aeb5d0434d67ca1ea368",
-  measurementId: "G-9V0M9VQDGM"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
