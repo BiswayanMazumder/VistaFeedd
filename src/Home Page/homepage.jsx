c6b7ac95e58438b50b80faf1113f55d0ac250684
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-27 13:56:13 +0530

    sidebar creation

--- src/Home Page/homepage.jsx
+++ src/Home Page/homepage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import Sidebar from '../Components/sidebar'
 import Sidebar_Home from '../Components/sidebar'
 
 export default function Homepage() {
