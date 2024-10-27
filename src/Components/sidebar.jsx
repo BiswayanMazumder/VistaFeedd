894dd72d51c4baed698752e250a038d6c8abeb04
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-27 13:40:03 +0530

    components created for the component

---
+++ src/Components/sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Sidebar() {
+  return (
+    <div className="jrnvfnvkfmv">
+                <Link>
+                <div className="jjvnkvn">
+                    <svg width="100" height="30" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="100" height="30" fill="black" rx="5" />
+                        <text
+                            x="50%"
+                            y="50%"
+                            fontFamily="'Lobster', cursive"
+                            fontSize="21"
+                            fill="white"
+                            textAnchor="middle"
+                            alignmentBaseline="middle"
+                            dominantBaseline="middle"
+                        >
+                            VistaFeedd
+                        </text>
+                    </svg>
+                </div>
+
+                </Link>
+
+            </div>
+  )
+}
--- src/Home Page/homepage.jsx
+++ src/Home Page/homepage.jsx
@@ -1,32 +1,14 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Sidebar from '../Components/sidebar'
 
 export default function Homepage() {
     useEffect(() => {
         document.title = "VistaFeedd"
     })
-  return (
-    <div className='webbody' style={{backgroundColor: 'black',display: 'flex', flexDirection:'row'}}>
-      <div className="jrnvfnvkfmv">
-      <div className="jjvnkvn">
-  <svg width="100" height="30" xmlns="http://www.w3.org/2000/svg">
-    <rect width="100" height="30" fill="black" rx="5" />
-    <text
-      x="50%"
-      y="50%"
-      fontFamily="'Lobster', cursive"
-      fontSize="21"
-      fill="white"
-      textAnchor="middle"
-      alignmentBaseline="middle"
-      dominantBaseline="middle"
-    >
-      VistaFeedd
-    </text>
-  </svg>
-</div>
-
-
-      </div>
-    </div>
-  )
+    return (
+        <div className='webbody' style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'row' }}>
+            <Sidebar/>
+        </div>
+    )
 }
