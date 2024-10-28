b5aaac90c689b4b3b9fca2d3a1e0435655577d99
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-28 16:57:40 +0530

    vercel analytics added

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ import Reels_Page from './Home Page/Reels_Page';
 import MessengerPage from './Home Page/Messengerpage';
 import NotificationPage from './Home Page/Notifiactionpage';
 import Profile_Page from './Home Page/Profile_Page';
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   // console.log('ENV',process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
   return (
     // <BrowserRouter>
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landingpage />} />
@@ -41,6 +43,7 @@ function App() {
       <Routes>
         <Route path="/profile" element={<Profile_Page />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
