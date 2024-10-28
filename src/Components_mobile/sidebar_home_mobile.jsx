4bade968da8276786032ea63e6e5cf0272100826
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-28 23:01:43 +0530

    Link router changes

--- src/Components_mobile/sidebar_home_mobile.jsx
+++ src/Components_mobile/sidebar_home_mobile.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Sidebar_home_mobile() {
     return (
         <div className="jjnd">
-            <Link style={{ textDecoration: 'none', color: 'white' }}>
+            <Link style={{ textDecoration: 'none', color: 'white' }} to={'/home'}>
                 <div className="hdjvkdv">
                     <svg aria-label="Home" class="x1lliihq x1n2onr6 x5n08af" fill="white" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path></svg>
                 </div>
