2e541fcf78c3da170743f3bab2ed3c75eb4fb494
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-11-07 19:53:43 +0530

    Verification badge will be shown in profile for verified users

--- src/Home Page/Sidebar Pages(Laptop)/profilepage_laptop.jsx
+++ src/Home Page/Sidebar Pages(Laptop)/profilepage_laptop.jsx
@@ -47,7 +47,7 @@ const Profilepage_laptop = () => {
 
         return () => unsubscribe(); // Cleanup subscription on unmount
     }, []);
-
+    const [verified,setverified] = useState(false);
     const fetchUserData = async (uid) => {
         const docRef = doc(db, "User Details", uid);
         const docSnap = await getDoc(docRef);
@@ -55,6 +55,7 @@ const Profilepage_laptop = () => {
             const data = docSnap.data();
             setProfilePicture(data['Profile Pic']);
             setName(data['Name']);
+            setverified(data['Verified']||false);
             setBio(data['Bio'] || 'No bio set');
         }
     };
@@ -151,6 +152,9 @@ const Profilepage_laptop = () => {
                 <div className="krmfvm">
                     <div className="mdnvmn" style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center' }}>
                         <div className="ddvbnd">{name}</div>
+                        {
+                            verified?<svg aria-label="Verified" class="x1lliihq x1n2onr6" fill="rgb(0, 149, 246)" height="18" role="img" viewBox="0 0 40 40" width="18"><title>Verified</title><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill-rule="evenodd"></path></svg>:<></>
+                        }
                         <Link style={{ textDecoration: 'none', color: "white" }} to={'/account/edit'}>
                             <div className="ddvbnd" style={{ height: "25px", width: "85px", borderRadius: "5px", backgroundColor: "grey", display: "flex", justifyContent: "center", alignItems: "center", textAlign: "center", fontSize: "12px" }}>
                                 Edit Profile
